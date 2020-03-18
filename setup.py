08cc6cf704a0b1f9040ca8345c007fa75851724d
xuesoso <yuanxue@stanford.edu>
2020-03-18 15:51:35 -0700

    updated

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='nheatmap',
-    version='0.1.0',
+    version='0.1.1',
     author='YSX',
     author_email='xuesoso@gmail.com',
     packages=setuptools.find_packages(),
@@ -12,7 +12,6 @@ setuptools.setup(
     long_description=open('README.md').read(),
     keywords = ['heatmap', 'hiearchical clustering', 'neat heatmap'],
     install_requires=[
-       "python >= 3.5",
        "matplotlib >= 3.0.3",
        "scipy >= 1.3.1",
        "numpy >= 1.17.2",
