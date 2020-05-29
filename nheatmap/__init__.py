9b89d2668258f01a58944e29e3388af139317353
xuesoso <yuanxue@stanford.edu>
2020-05-29 00:41:09 -0700

    0.1.3 version update

--- nheatmap/__init__.py
+++ nheatmap/__init__.py
@@ -1,2 +1,5 @@
 name = "nheatmap"
 from .plot import nheatmap as nhm
+
+__version__ = "0.1.3"
+nhm.__version__ = __version__
--- setup.py
+++ setup.py
@@ -1,8 +1,25 @@
 import setuptools
+import codecs
+import os.path
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+version = get_version('./nheatmap/__init__.py')
 
 setuptools.setup(
     name='nheatmap',
-    version='0.1.2',
+    version=version,
     author='YSX',
     author_email='xuesoso@gmail.com',
     packages=setuptools.find_packages(),
